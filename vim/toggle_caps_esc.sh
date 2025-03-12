b6324ae54b730565d154686b6a97048def81542b
Achraf Khadraoui <achraf.thegeek@gmail.com>
2025-03-12 08:30:37 +0000

    caps to esc toggle for vim

---
+++ vim/toggle_caps_esc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Check if caps:swapescape is currently active
+CURRENT=$(setxkbmap -query | grep -c "caps:swapescape")
+if [ "$CURRENT" -eq 1 ]; then
+	# if active, reset to default
+	setxkbmap -option
+	notify-send "Caps Lock and Esc reset to default"
+else
+	#if not active, swap Caps Lock and Esc
+	setxkbmap -option caps:swapescape
+	notify-send "Caps Lock and Esc swapped"
+fi

b852a9cd1a9df4821841064fc714cdd6849ac69a
Achraf Khadraoui <75825038+AchrafTheGeek@users.noreply.github.com>
2026-03-03 21:46:32 +0000

    updated script and guide for vscode vim-motions

--- vim/toggle_caps_esc.sh
+++ vim/toggle_caps_esc.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Check if caps:swapescape is currently active
-CURRENT=$(setxkbmap -query | grep -c "caps:swapescape")
+CURRENT=$(setxkbmap -query | grep -c "caps:escape_shifted_capslock")
 if [ "$CURRENT" -eq 1 ]; then
 	# if active, reset to default
 	setxkbmap -option
 	notify-send "Caps Lock and Esc reset to default"
 else
 	#if not active, swap Caps Lock and Esc
-	setxkbmap -option caps:swapescape
+	setxkbmap -option "caps:escape_shifted_capslock"
 	notify-send "Caps Lock and Esc swapped"
 fi
